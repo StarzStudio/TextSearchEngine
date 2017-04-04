46effc2386ed24804a602719de5f80e73833c1d3
Xing Zhou <cszxing@gmail.com>
2017-04-04 17:30:53 -0400

    let Client receive arguments from command line

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -41,18 +41,17 @@ int Client::initFileMgrComponent() {
 }
 
 
-int main()
+int main(int argc, char *argv[])
 {
-
-
+	std::string path = argv[2];
+	std::string stringToBeSearched = argv[1];
 	Client c;
-	std::string path = "..";
-	std::string pattern1 = "*.h";
-	std::string pattern2 = "*.cpp";
-	std::string stringToBeSearched = "algorithm";
 	c.addPath(path);
-	c.addPattern(pattern1);
-	c.addPattern(pattern2);
+	for (int i = 3; i < argc; i++)
+	{
+		c.addPattern(argv[i]);
+	}
+
 	c.provideTextSearchEngineStringToSearch(stringToBeSearched);
 	c.search();
 
---
+++ Complie.ps1
@@ -0,0 +1,3 @@
+devenv TextSearch.sln /rebuild debug
+regsvr32 /s "/Debug\TextSearchComponent.dll"
+regsvr32 /s "/Debug\FileMgrComponent.dll"
